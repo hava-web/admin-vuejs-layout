273eae1004d447ed00b1fd0a43ea860f9e5c1555
unknown <nguyenngocha18062002@gmail.com>
2022-12-22 12:49:27 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import TableVue from './components/table.vue';
+</script>
+
+<template>
+  <h1>Hello</h1>
+  <h2>
+    <TableVue />
+  </h2>
+</template>
+
+
